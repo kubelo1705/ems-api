11bfa27e049231c6c6f825d9cfda15fdc2e07981
Phat Tran <phat.tran.210700@gmail.com>
2022-02-24 16:33:28 +0700

    second push

--- src/main/java/com/example/managedevices/entity/Credential.java
+++ src/main/java/com/example/managedevices/entity/Credential.java
@@ -1,7 +1,18 @@
 package com.example.managedevices.entity;
 
-import javax.persistence.Entity;
+import lombok.*;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
 @Entity
+@Table
 public class Credential {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
 }
